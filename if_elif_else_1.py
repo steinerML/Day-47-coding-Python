5a880122b54643c38951c9010318d2524eb94a8c
steinerML <102678863+steinerML@users.noreply.github.com>
2023-02-09 13:09:59 +0100

    Update if_elif_else_1.py

--- if_elif_else_1.py
+++ if_elif_else_1.py
@@ -28,3 +28,65 @@ else:
     msg = 'Elder'
 
 print(msg)
+
+#Using UPPER INTERVALS
+
+""" person < 2 (Baby)
+    person >= 2 and < 4 (Toddler)
+    person >= 4 and < 13 (Kid)
+    person >= 13 and < 20 (Teenager)
+    person >= 20 and < 65 (Adult)
+    person >= 65 (Elder) """
+
+age = int(input("Enter your age: "))
+
+if age < 2:
+    msg = 'Baby'
+
+elif age < 4:
+    msg = 'Toddler'
+
+elif age < 13:
+    msg = 'Kid'
+
+elif age < 20:
+    msg = 'Teenager'
+
+elif age < 65:
+    msg = 'Adult'
+
+else:
+    msg = 'Elder'
+
+print(msg)
+
+#Using LOWER INTERVALS
+
+""" person < 2 (Baby)
+    person >= 2 and < 4 (Toddler)
+    person >= 4 and < 13 (Kid)
+    person >= 13 and < 20 (Teenager)
+    person >= 20 and < 65 (Adult)
+    person >= 65 (Elder) """
+
+age = int(input("Enter your age: "))
+
+if age >= 65:
+    msg = 'Elder'
+
+elif age >= 20:
+    msg = 'Adult'
+
+elif age >= 13:
+    msg = 'Teenager'
+
+elif age >= 4:
+    msg = 'Kid'
+
+elif age >= 2:
+    msg = 'Toddler'
+
+else:
+    msg = 'Baby'
+
+print(msg)
